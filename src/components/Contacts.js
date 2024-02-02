342213e597287f8da9d9f0b71a0a4fe8baf186e9
Gustavo Takahashi <gustavokenjitakahashi@gmail.com>
2024-02-02 00:14:47 -0300

    changes in contact form

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from 'react'
+import React, { useRef, useState, useEffect, createElement } from 'react'
 import emailjs from '@emailjs/browser'
 import Button from '@mui/material/Button'
 import TextField from '@mui/material/TextField'
@@ -9,131 +9,88 @@ const Contacts = () => {
   const cursorName = useRef()
   const cursorEmail = useRef()
   const cursorMessage = useRef()
+  const emailMirror = useRef()
+  const messageMirror = useRef()
+  const nameMirror = useRef()
   const w = window.innerWidth
-  const [clicked, setClicked] = useState(false)
   const system = window.navigator.userAgentData.platform
+
   var cursorNameStatus = true
-  var cursorEmailStatus = false
-  var cursorMessageStatus = false
   const nameTextArea = useRef()
+
+  var cursorEmailStatus = false
   const emailTextArea = useRef()
+
+  var cursorMessageStatus = false
   const messageTextArea = useRef()
-  const nameCursorPos = nameTextArea.current?.selectionStart
-  const emailCursorPos = emailTextArea.current?.selectionStart
-  const messageCursorPos = messageTextArea.current?.selectionStart
-  const nameTextBeforeCursor = nameTextArea.current?.value.substring(
-    0,
-    nameCursorPos
-  )
-  const nameTextAfterCursor =
-    nameTextArea.current?.value.substring(nameCursorPos)
-  const emailTextBeforeCursor = emailTextArea.current?.value.substring(
-    0,
-    emailCursorPos
-  )
-  const emailTextAfterCursor =
-    emailTextArea.current?.value.substring(emailCursorPos)
-  const messageTextBeforeCursor = messageTextArea.current?.value.substring(
-    0,
-    messageCursorPos
-  )
-  const messageTextAfterCursor =
-    messageTextArea.current?.value.substring(messageCursorPos)
-
-  console.log(nameTextBeforeCursor)
-  console.log(emailTextBeforeCursor)
-
-  console.log(messageTextBeforeCursor)
 
   const sendEmail = (e) => {
     e.preventDefault()
-    emailjs
-      .sendForm(
-        'service_ozj7ubn',
-        'template_ye4n9jk',
-        form.current,
-        'jLdOxN9R4ESMRHdhC'
-      )
-      .then(
-        (result) => {
-          console.log(result.text)
-        },
-        (error) => {
-          console.log(error.text)
-        }
-      )
+    emailjs.sendForm('service_ozj7ubn', 'template_ye4n9jk', form.current, 'jLdOxN9R4ESMRHdhC').then(
+      (result) => {
+        console.log(result.text)
+      },
+      (error) => {
+        console.log(error.text)
+      }
+    )
   }
 
-  const handleChange = (e) => {
-    console.log(e)
+  const handleSelectionChange = (area, mirror) => {
+    const cursorPos = area.current?.selectionStart
+    const textBeforeCursor = area.current?.value.substring(0, cursorPos)
+    const textAfterCursor = area.current?.value.substring(cursorPos)
+    const careEle = <span className="cursor">&nbsp;</span>
   }
 
   // useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     if (cursorStatus) {
-  //       cursor.current.style.opacity = 0
-  //       cursorStatus = false
-  //     } else {
-  //       cursor.current.style.opacity = 1
-  //       cursorStatus = true
-  //     }
-  //   }, 300)
-  //   return () => clearInterval(interval)
+  //   const textAreaStyles = window.getComputedStyle(messageTextArea.current)
   // }, [])
+
   return (
     <Box className="relative z-10 top-[40%] md:left-[25%] md:w-1/2 w-auto">
       <p
         className={`bg-[#555753] text-[#EEEEEC] ml-7 mr-7 text-center rounded-tl-lg border-b-2 rounded-tr-lg p-2 mb-0  md:text-lg text-sm`}>
-        user@{system.toLowerCase()}-{isMobile ? 'mobile' : 'computer'}:
-        /contacts/send_email
+        user@{system.toLowerCase()}-{isMobile ? 'mobile' : 'computer'}: /contacts/send_email
       </p>
-      <p
-        className={`bg-white text-sm text-black text-center m-7 border-b-2 mb-0 mt-0 md:text-lg`}>
+      <p className={`bg-white text-sm text-black text-center m-7 border-b-2 mb-0 mt-0 md:text-lg`}>
         {/* <span>GNU Nano 4.3</span> */}
         <span className="">File: form.txt</span>
       </p>
       <Box
         component="form"
         onSubmit={sendEmail}
-        onMouseEnter={() => {
-          setClicked(true)
-        }}
-        onMouseLeave={() => {
-          setClicked(false)
-        }}
         ref={form}
         className={`bg-[#2e3436] flex text-white flex-col m-7 gap-2 rounded-bl-lg rounded-br-lg p-4 mt-0`}
         noValidate
         autoComplete="off">
-        <label className="flex text-lg items-center">
-          nome=
-          <span className="text-3xl" ref={cursorName}>
-            ∎
-          </span>
+        <label className="flex text-lg items-center relative">
+          nome =
+          <div className="absolute top-0 left-0 h-full w-full overflow-hidden text-transparent" ref={nameMirror}></div>
           <input
             autoFocus
             name={'user_name'}
             ref={nameTextArea}
-            onChange={handleChange}
+            onClick={() => handleSelectionChange(nameTextArea, nameMirror)}
             className="flex-1 bg-[#2e3436] border-none text-lg text-white"></input>
         </label>
-        <label className="flex text-lg items-center">
-          email=
-          <span className="text-3xl" ref={cursorEmail}>
-            ∎
-          </span>
+        <label className="flex text-lg items-center relative">
+          email =
+          <div className="absolute top-0 left-0 h-full w-full overflow-hidden text-transparent" ref={emailMirror}></div>
           <input
             name={'user_email'}
             ref={emailTextArea}
+            onClick={() => handleSelectionChange(emailTextArea, emailMirror)}
             className="flex-1 bg-[#2e3436] border-none text-lg text-white"></input>
         </label>
-        <label className="flex text-lg items-center">
-          mensagem=
-          <span className="text-3xl" ref={cursorEmail}>
-            ∎
-          </span>
+        <label className="flex text-lg items-center relative">
+          mensagem ={' '}
+          <div
+            className="absolute top-0 left-0 h-full w-full overflow-hidden text-transparent"
+            ref={messageMirror}></div>
           <textarea
             name={'message'}
+            onClick={() => handleSelectionChange(messageTextArea, messageMirror)}
             ref={messageTextArea}
             rows={3}
             className="flex-1 bg-[#2e3436] border-none text-lg text-white"></textarea>
@@ -169,11 +126,7 @@ const Contacts = () => {
           multiline
           rows={4}
           name={'message'}></TextField> */}
-        <Button
-          type="submit"
-          variant="contained"
-          color="primary"
-          className="md:w-1/2 md:self-center">
+        <Button type="submit" variant="contained" color="primary" className="md:w-1/2 md:self-center">
           Enviar
         </Button>
       </Box>
