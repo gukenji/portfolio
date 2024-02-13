e3fd57fa320db2316eb4608fa4deeba82356df53
Gustavo Takahashi <gustavokenjitakahashi@gmail.com>
2024-02-13 20:17:11 -0300

    fix speed insights

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
-import { SpeedInsights } from '@vercel/speed-insights/next'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import { Analytics } from '@vercel/analytics/react'
 
 function App() {
